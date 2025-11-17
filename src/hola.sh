d74cfb9da3a934b6590d1d2df34f6c61e481cac7
Joao Patacho <patacholasprilla23@iespabloserrano.com>
2025-11-17 14:18:05 +0100

    prueba de ejercicio

--- src/hola.sh
+++ src/hola.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Hola"
+name: Ejecutar script hola
+
+on:
+  push:
+ 
+jobs:
+  ejecutar-hola:
+	runs_on: ubuntu-latest
+	  
+	steps:
+	  - name: Checkout del repositorio
+		 uses: actions/checout@v5
+		  
+	  - name:Dar permisos de ejecución
+		run:chmod +x src/hola.sh
+		
+	  - name:Ejecutar script
+        run: ./src/hola.sh
+		
